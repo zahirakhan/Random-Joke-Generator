314a1228af689a9bc0c1cf6bbc4a6bf938bd675b
Zahira Khan <66169164+zahirakhan@users.noreply.github.com>
2025-04-06 14:00:39 +0500

    joke_generator.py

--- joke_generator.py
+++ joke_generator.py
@@ -6,10 +6,25 @@ def fetch_joke():
     response = requests.get(url)
     joke = response.json()
     return f"{joke['setup']} - {joke['punchline']}"
-
 st.title("Random Joke Generator 😄")
 st.write("Click the button to get a random joke! 🤔")
 
 if st.button('Get Joke 🤣'):
     joke = fetch_joke()
-    st.write(f"Here's your joke: {joke} 😆")
+   
+    st.markdown(f"""
+    <style>
+    .joke-text {{
+        font-size: 24px;
+        color: #2980b9;
+        text-align: center;
+        font-weight: bold;
+        margin-top: 20px;
+        background-color: #ecf0f1;
+        padding: 15px;
+        border-radius: 10px;
+        box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+    }}
+    </style>
+    """, unsafe_allow_html=True)
+    st.markdown(f'<div class="joke-text">{joke} 😆</div>', unsafe_allow_html=True)
